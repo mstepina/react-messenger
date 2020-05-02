1c01c1db98650326d89ebf28cf04a754a7021263
mstepina <margarita.stepina@gmail.com>
2020-05-02 18:55:20 -0400

    rendering dialog list

--- src/components/DialogList.tsx
+++ src/components/DialogList.tsx
@@ -21,16 +21,16 @@ export let dialogList : DialogList = [
     {
         name: 'Shoobsters',
         image:"https://image.flaticon.com/icons/svg/146/146025.svg",
-        date: new Date ("29/04/2020"),
+        date: new Date (Date.UTC(2012, 11, 12, 3, 0, 0)),
         author: "Rita",
         message: "Henlo!"
     },
     {
         name: 'Corgos',
         image:"https://image.flaticon.com/icons/svg/146/146025.svg",
-        date: new Date ("30/04/2020"),
+        date: new Date (Date.UTC(2020, 11, 12, 3, 0, 0)),
         author: "Tofu",
-        message: "Henlo!"
+        message: "Henlo! what are you doing silly billy wiille. You can use the overflow property when you want to have better control of the layout. The overflow property specifies what happens if content overflows an element's box"
     }
 
 ]
@@ -49,17 +49,47 @@ export const taskTestData: Props = {
 // }
 
 export const DialogsComponent = (props: Props) => {
-   
-   
-
-
     
-    return (<div style={style}>
+    
+    
+    
+    
+    function formatDate(date: Date) {
+        const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
+        return date.toLocaleDateString(undefined, options).replace(/,/g, " ");
+    
+    } 
+    return (
+    <div className="dialog-list">
+        <h3 className="heading">Dialogs</h3>
+            {props.item.map(p => {
+                return (
+                <div className="message-box">
+                    <span id="icon"><img src={p.image} alt="userpic"/></span>
+                    <div className="text">
+                        <h5>{p.name}<span id="date">{formatDate(p.date)}</span></h5>
+                        <p>{p.author}: <span id ="mess">{p.message}</span></p>
+                    </div>
+                    
+                
+                    {/* <p id="name">{p.name}</p>
+                    <p id="date">{formatDate(p.date)}</p>
+                    <p id="author">{p.author}</p>
+                    <p >{p.message}</p>  */}
+                    
+                </div>
+                )
+            })}
+    </div>)
+    }
+    
+    /*
+    return (<div className="dialog-list" style={style}>
         {props.item[0].name}
         <br/>
         <div>
         {props.item[0].author}
         </div>
     </div>)
-    
-}
+    */
+
